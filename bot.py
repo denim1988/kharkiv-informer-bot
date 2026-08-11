71240ceb34ad749924de1d7aee7cc43ef20428b0
denim1988 <de-denim@ukr.net>
2026-08-11 09:03:26 +0300

    основной бот

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8080)
+
+threading.Thread(target=run_flask, daemon=True).start()
 import time
 import requests
 import telebot
